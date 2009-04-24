e3b07bbf74c1245cda70f175c83f61d5d6171f03
Samuel Stauffer <samuel@descolada.com>
2009-04-23 22:20:11 -0700

    Added code from Django to automatically build the package list in setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,48 @@
 #!/usr/bin/env python
 
+import os
 from distutils.core import setup
+
+# Borrowed from Django:
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+django_dir = 'gypsy'
+
+for dirpath, dirnames, filenames in os.walk(django_dir):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
 setup(
     name = 'gypsy',
-    version = '1.0.1',
+    version = '1.0.2',
     description = 'Django extensions by Lefora',
     author = 'Lefora',
     author_email = 'samuel@lefora.com',
     url = 'http://github.com/samuel/gypsy/tree/master',
-    packages = [
-        'gypsy',
-        'gypsy.auth',
-        'gypsy.sessions',
-        'gypsy.storage',
-        'gypsy.storage.backends',
-        'gypsy.utils',
-    ],
+    packages = packages,
+    data_files = data_files,
 )
