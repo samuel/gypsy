6ea861259004a32269aa618ea9b3cee3b8dd0a66
Samuel Stauffer <samuel@descolada.com>
2009-04-23 22:08:38 -0700

    Added all the packages to setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,17 @@ from distutils.core import setup
 
 setup(
     name = 'gypsy',
-    version = '1.0.0',
+    version = '1.0.1',
     description = 'Django extensions by Lefora',
     author = 'Lefora',
     author_email = 'samuel@lefora.com',
     url = 'http://github.com/samuel/gypsy/tree/master',
-    packages = ['gypsy'],
+    packages = [
+        'gypsy',
+        'gypsy.auth',
+        'gypsy.sessions',
+        'gypsy.storage',
+        'gypsy.storage.backends',
+        'gypsy.utils',
+    ],
 )
